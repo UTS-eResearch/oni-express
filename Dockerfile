910f2564ce66e2253c7c09fe4d5e5aae0a5c7499
Moises Sacal <moisbo@gmail.com>
2020-10-27 16:20:24 +1100

    removed need to compile docker image

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,10 @@
 FROM node:10
 
-VOLUME ["/etc/share/ocfl","/etc/share/logs","/etc/share/config"]
+VOLUME [ "/etc/share/ocfl","/etc/share/logs","/etc/share/config", "/usr/src/app" ]
 
 # Install the express app first
 
 WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
 EXPOSE 8080
 
-# Fetch the frontend repo, oni-portal
-
-WORKDIR /usr/src/build
-RUN git clone -b feature-expert-nation https://github.com/UTS-eResearch/oni-portal.git
-
-# Go back to oni-portal and build it 
-
-WORKDIR /usr/src/build/oni-portal
-RUN npm install
-RUN npm run build
-RUN mkdir -p /usr/src/app
-RUN cp -r /usr/src/build/oni-portal/dist /usr/src/app/portal
-
-# entry point
-
-WORKDIR /usr/src/app
-
-CMD [ "npm", "start" ]
+CMD [ "npm", "run", "buildAndStart" ]
--- docker-run.sh
+++ docker-run.sh
@@ -5,6 +5,7 @@
 
 DCK_PRE=en #DCK_PRE or docker prefix to identify docker group
 VOL_OCFL="/Users/moises/source/github/uts-eresearch/heurist2ro-crate/en_ocfl/"
+VOL_APP=$(pwd)
 VOL_CONFIG="$(pwd)/config/"
 
 PORT=8080
@@ -20,11 +21,11 @@ memcached
 
 docker run --rm -p 127.0.0.1:${PORT}:${PORT} \
 -e NODE_ENV=development \
+-v ${VOL_APP}:/usr/src/app \
 -v ${VOL_CONFIG}:/etc/share/config  \
 -v ${VOL_OCFL}:/etc/share/ocfl \
 --name ${DCK_PRE}-oni-express \
 --network ${NETWORK} \
--d \
 oni-express
 
 # For MacOS creating a docker volume that we can identify
@@ -33,7 +34,7 @@ oni-express
 # and chmod 8983:8983 ${VOL_SOLR} directory
 # more here https://docs.docker.com/docker-for-mac/osxfs/
 
-docker volume create solr_ocfl
+docker volume create ${DCK_PRE}-solr_ocfl
 
 docker run --rm -p 127.0.0.1:8983:8983 \
 -v solr_ocfl:/var/solr:delegated \
@@ -42,3 +43,5 @@ docker run --rm -p 127.0.0.1:8983:8983 \
 -d \
 solr:8 \
 solr-precreate ocfl
+
+echo "open http://localhost:${PORT}"
